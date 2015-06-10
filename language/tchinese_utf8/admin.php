bfac2585f24192a99f5953ea2840cf3d4e6e3916
Tad <tad0616@gmail.com>
2015-06-10 14:33:38 +0800

    調整語系

--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -5,16 +5,14 @@
 // $Id:$
 // ------------------------------------------------------------------------- //
 
-include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
-define("_TAD_NEED_TADTOOLS"," 需要 tadtools 模組，可至<a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad教材網</a>下載。");
+include_once '../../tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
+define('_TAD_NEED_TADTOOLS', ' 需要 modules/tadtools，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
 
-define("_MA_TADLUNCH2_LUNCH_SN","編號");
-define("_MA_TADLUNCH2_LUNCH_TITLE","供餐來源");
-define("_MA_TADLUNCH2_LUNCH_FACTORY","廠商名稱");
-define("_MA_TADLUNCH2_LUNCH_DIETICIAN","營養師");
-define("_MA_TADLUNCH2_LUNCH_FACTORY_TEL","廠商電話");
-define("_MA_TADLUNCH2_LUNCH_FACTORY_FAX","廠商傳真");
-define("_MA_TADLUNCH2_LUNCH_FACTORY_ADDR","廠商地址");
-define("_MA_TAD_LUNCH2_FORM","輸入表單");
-
-?>
+define("_MA_TADLUNCH2_LUNCH_SN", "編號");
+define("_MA_TADLUNCH2_LUNCH_TITLE", "供餐來源");
+define("_MA_TADLUNCH2_LUNCH_FACTORY", "廠商名稱");
+define("_MA_TADLUNCH2_LUNCH_DIETICIAN", "營養師");
+define("_MA_TADLUNCH2_LUNCH_FACTORY_TEL", "廠商電話");
+define("_MA_TADLUNCH2_LUNCH_FACTORY_FAX", "廠商傳真");
+define("_MA_TADLUNCH2_LUNCH_FACTORY_ADDR", "廠商地址");
+define("_MA_TAD_LUNCH2_FORM", "輸入表單");
